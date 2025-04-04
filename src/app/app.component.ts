8d26e49bc0824d29afed82bcee8c7d4df1df1478
Jucalino <joseluis.bmjnet@gmail.com>
2025-04-04 09:55:17 -0300

    @Input e @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { HomeComponent } from './components/home/home.component';
 })
 export class AppComponent {
   title = 'appAngular';
+
+  
+  logar(event: string){
+    console.log(event)
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { EnviaFormularioService } from '../../services/envia-formulario.service';
 
 @Component({
@@ -13,8 +13,14 @@ export class HomeComponent {
   deveMostrarTitulo = true
   name = "Juca"
   listItems = ["Juca", "Duda", "Belms"]
+  
+  @Input("name") minhaPropsDeFora!: string;
+
+  @Output() emitindoValorName = new EventEmitter<string>()
 
   submit(){
+    this.emitindoValorName.emit(this.name)
     this.enviaFormularioService.enviaInformacaoParaBackend("Enviando formacao")
   }
+
 }
